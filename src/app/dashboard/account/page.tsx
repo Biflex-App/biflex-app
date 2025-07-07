2ec9069f16d92fde557c362ec3e38056020afb58
Aemielvin Loremia <aemielvin.loremia@gmail.com>
2025-07-08 06:09:53 +0800

    Add placeholder pages

---
+++ src/app/dashboard/account/page.tsx
@@ -0,0 +1,5 @@
+export default function AccountPage() {
+  return (
+    <h1>The Account feature is still in development. Stay tuned!</h1>
+  );
+}
---
+++ src/app/dashboard/workouts/page.tsx
@@ -0,0 +1,5 @@
+export default function WorkoutsPage() {
+  return (
+    <h1>The Workouts feature is still in development. Stay tuned!</h1>
+  );
+}
