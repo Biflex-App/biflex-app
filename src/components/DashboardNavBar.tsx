1ca5f7e4753d5455278340c29940d92b95ccee20
Aemielvin Loremia <aemielvin.loremia@gmail.com>
2025-07-08 06:23:37 +0800

    Update dashboardnavbar to highlight current active page

--- src/components/DashboardNavBar.tsx
+++ src/components/DashboardNavBar.tsx
@@ -1,4 +1,7 @@
+"use client"
+
 import Link from "next/link";
+import { useSelectedLayoutSegment } from "next/navigation";
 import { Home, Calendar, Dumbbell, Users, User } from "lucide-react";
 import {
   NavigationMenu,
@@ -36,21 +39,27 @@ const navItems = [
 ];
 
 export default function DashboardNavBar() {
+  const segment = useSelectedLayoutSegment();
+
   return (
     <NavigationMenu className="fixed bottom-0 w-full z-50 flex justify-center rounded-none border-0 border-t-2">
       <NavigationMenuList className="flex justify-center items-center w-screen">
-        {navItems.map(({ label, href, icon: Icon }) => (
-          <NavigationMenuItem key={label}>
-            <Link href={href} passHref>
-              <NavigationMenuLink className="flex flex-col items-center">
-                <Icon className="w-6 h-6 mb-1" />
-                <span className="text-xs font-bold tracking-wide">
-                  {label}
-                </span>
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-        ))}
+        {navItems.map(({ label, href, icon: Icon }) => {
+          const isActive = (href === '/dashboard' && segment === null)
+            || (segment === href.split('/').pop());
+          return (
+            <NavigationMenuItem key={label}>
+              <Link href={href} passHref>
+                <NavigationMenuLink className={`flex flex-col items-center w-16 ${isActive ? 'bg-blue-100' : ''}`}>
+                  <Icon className="w-6 h-6 mb-1" />
+                  <span className="text-xs font-bold tracking-wide">
+                    {label}
+                  </span>
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          );
+        })}
       </NavigationMenuList>
     </NavigationMenu>
   );
