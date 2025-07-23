4b2214e9dcf4699e06cea126697bd0df1d762707
Aemielvin Loremia <aemielvin.loremia@gmail.com>
2025-07-23 17:11:26 +0800

    Limit displayed amount of elements

--- src/components/ExerciseListPage.tsx
+++ src/components/ExerciseListPage.tsx
@@ -1,9 +1,15 @@
 'use client';
 
 import { useExercises } from "@/hooks/exercise";
+import { useState } from "react";
 
 export default function ExerciseListPage() {
+  const [displayLength, setDisplayLength] = useState(10);
   const { data: exercises, isLoading } = useExercises();
+
+  function increaseDisplayLength() {
+    setDisplayLength(Math.min(displayLength + 10, exercises?.length ?? 0));
+  }
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -12,7 +18,7 @@ export default function ExerciseListPage() {
   return (
     <div>
       <h1>Exercise List</h1>
-      {exercises?.map((exercise) => (
+      {exercises?.slice(0, displayLength).map((exercise) => (
         <div key={exercise._id}>
           <h2>{exercise.name}</h2>
         </div>
